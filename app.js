5e08076854d41cbb4e370d689621b2ec2888752d
romak maharjan <romakmaharjan@gmail.com>
2023-06-20 16:30:09 +0545

    Add exisiting project files to git

---
+++ app.js
@@ -0,0 +1,115 @@
+// /* -----------------------------------------------
+// /* How to use? : Check the GitHub README
+// /* ----------------------------------------------- */
+
+// /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+// /*
+// particlesJS.load('particles-js', 'particles.json', function() {
+//   console.log('particles.js loaded - callback');
+// });
+// */
+
+// /* Otherwise just put the config content (json): */
+
+// particlesJS("particles-js", {
+//   particles: {
+//     number: {
+//       value: 80,
+//       density: {
+//         enable: true,
+//         value_area: 800,
+//       },
+//     },
+//     color: {
+//       value: ["#ff7979", "#B8E986", "#50E3C2"],
+//     },
+//     shape: {
+//       type: "traingle",
+//       stroke: {
+//         width: 0,
+//         color: "#b6b2b2",
+//       },
+//     },
+//     opacity: {
+//       value: 1,
+//       random: false,
+//       anim: {
+//         enable: true,
+//         speed: 1,
+//         opacity_min: 0.1,
+//         sync: false,
+//       },
+//     },
+//     size: {
+//       value: 10,
+//       random: true,
+//       anim: {
+//         enable: true,
+//         speed: 12.181158184520175,
+//         size_min: 0.1,
+//         sync: true,
+//       },
+//     },
+//     line_linked: {
+//       enable: true,
+//       distance: 32,
+//       color: "#b6b2b2",
+//       opacity: 1,
+//       width: 4.4,
+//     },
+//     move: {
+//       enable: true,
+//       speed: 1,
+//       direction: "none",
+//       random: true,
+//       straight: false,
+//       out_mode: "bounce",
+//       bounce: false,
+//       attract: {
+//         enable: false,
+//         rotateX: 600,
+//         rotateY: 1200,
+//       },
+//     },
+//   },
+//   interactivity: {
+//     detect_on: "canvas",
+//     events: {
+//       onhover: {
+//         enable: false,
+//         mode: "repulse",
+//       },
+//       onclick: {
+//         enable: false,
+//         mode: "push",
+//       },
+//       resize: true,
+//     },
+//     modes: {
+//       grab: {
+//         distance: 400,
+//         line_linked: {
+//           opacity: 1,
+//         },
+//       },
+//       bubble: {
+//         distance: 400,
+//         size: 60,
+//         duration: 2,
+//         opacity: 8,
+//         speed: 3,
+//       },
+//       repulse: {
+//         distance: 200,
+//         duration: 0.4,
+//       },
+//       push: {
+//         particles_nb: 4,
+//       },
+//       remove: {
+//         particles_nb: 2,
+//       },
+//     },
+//   },
+//   retina_detect: true,
+// });
